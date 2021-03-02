7fa0538188e4b0d1de2b464cbc26b9d656e3130a
Israel Filho <israelfilhocpd@gmail.com>
2021-03-02 09:54:38 -0300

    learning class in kotlin

---
+++ Kotlin/classes/Pessoa.kt
@@ -0,0 +1,21 @@
+
+
+class Pessoa (){
+    
+    var nome:String = name;
+    var cpf:String = "xxx.xxx.xxx-xx";
+
+}
+
+
+fun main(){
+    val israel = Pessoa();
+
+    println(israel.nome);
+    println(israel.cpf);
+
+}
+
+
+
+
